8229ded5cd2afb094ad271e83b44973ac12082e3
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-03-02 19:53:54 +0200

    Update: move logout functionality to side menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,15 +4,8 @@ import { LoginService } from '../login.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
-
-  constructor(private login: LoginService) {
-
-  }
-
-  logout() {
-    this.login.logout();
-  }
+  constructor() {}
 }
