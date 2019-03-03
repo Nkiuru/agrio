997575fd9dc47d24a97fce188cfa60f0d42af1e5
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-03-03 18:16:10 +0200

    Add: status bar service

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,36 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
+import { StatusBarService } from '../status-bar.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
   username = '';
   password = '';
 
-  constructor(private loginService: LoginService, private router: Router) {
-  }
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private statusBar: StatusBarService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.statusBar.setToLight();
   }
 
   submit() {
     if (this.username.length > 0 && this.password.length > 0) {
-      this.loginService.logIn(this.username, this.password).subscribe((data) => {
-        console.log(data);
-        localStorage.setItem('token', data.token);
-        localStorage.setItem('user', JSON.stringify(data.user));
-        this.loginService.isLoggedIn = true;
-        this.router.navigate(['']).catch(err => console.log(err));
-      }, error => console.log(error));
+      this.loginService.logIn(this.username, this.password).subscribe(
+        data => {
+          console.log(data);
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('user', JSON.stringify(data.user));
+          this.loginService.isLoggedIn = true;
+          this.router.navigate(['']).catch(err => console.log(err));
+        },
+        error => console.log(error)
+      );
     }
   }
 
   signUp() {
     this.router.navigate(['/sign-up']).catch(err => console.log(err));
   }
-
 }
