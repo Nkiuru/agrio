d7828dabf889108fd78c62c9455072960f793052
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-03 14:25:00 +0200

    Fix back button behavior on native

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoginService } from '../login.service';
+import { Platform, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +7,34 @@ import { LoginService } from '../login.service';
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
-  constructor() {}
+  subscription: any;
+
+  constructor(private platform: Platform, private toastCtrl: ToastController) {
+  }
+
+  ionViewDidEnter() {
+    let lastTimeBackPress = 0;
+    let timePeriodToExit = 2000;
+    this.subscription = this.platform.backButton.subscribe(() => {
+      if (new Date().getTime() - lastTimeBackPress < timePeriodToExit) {
+        navigator['app'].exitApp();
+      } else {
+        this.showToast().catch(err => console.log(err));
+        lastTimeBackPress = new Date().getTime();
+      }
+    });
+  }
+
+  ionViewWillLeave() {
+    this.subscription.unsubscribe();
+  }
+
+  async showToast() {
+    let toast = await this.toastCtrl.create({
+      message: 'Press back again to exit App',
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 }
