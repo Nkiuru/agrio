76ad967feb17e9d12989dba36b47d3c41e8a012e
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-03-03 12:04:07 +0200

    Update: status bar color to use theme colors

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+
+  constructor(private statusBar: StatusBar) {}
+
+  ionViewWillEnter() {
+    // switch status bar color to white icons and green background
+    this.statusBar.styleLightContent();
+    this.statusBar.backgroundColorByHexString('#005944');
+  }
+
+}
