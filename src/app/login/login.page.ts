4e8d2530d3e493f5790d2daa3decf828f4ee78dc
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-04 14:19:32 +0200

    #27 show login failure to user

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LoginService } from '../login.service';
 import { Router } from '@angular/router';
 import { StatusBarService } from '../status-bar.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,14 +12,19 @@ import { StatusBarService } from '../status-bar.service';
 export class LoginPage implements OnInit {
   username = '';
   password = '';
+  @ViewChild('uname') usernameElm: ElementRef;
+  @ViewChild('passwd') passwordElm: ElementRef;
 
   constructor(
     private loginService: LoginService,
     private router: Router,
-    private statusBar: StatusBarService
-  ) {}
+    private statusBar: StatusBarService,
+    private toast: ToastController
+  ) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
   ionViewWillEnter() {
     this.statusBar.setToLight();
@@ -34,12 +40,32 @@ export class LoginPage implements OnInit {
           this.loginService.isLoggedIn = true;
           this.router.navigate(['']).catch(err => console.log(err));
         },
-        error => console.log(error)
+        error => {
+          const message: string = error.error.message;
+          this.showToast(message);
+          if (message.includes('username')) {
+            // @ts-ignore
+            this.usernameElm.el.classList.add('ion-invalid');
+          }
+          if (message.includes('password')) {
+            // @ts-ignore
+            this.passwordElm.el.classList.add('ion-invalid');
+          }
+        }
       );
     }
   }
 
   signUp() {
     this.router.navigate(['/sign-up']).catch(err => console.log(err));
+  }
+
+  async showToast(message: string) {
+    let toast = await this.toast.create({
+      message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 }
