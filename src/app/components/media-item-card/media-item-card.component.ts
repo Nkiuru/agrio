6091d37306b7dc132b9f10ad5fc237d36053fbda
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2019-03-04 19:16:30 +0200

    Add: media item component

---
+++ src/app/components/media-item-card/media-item-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-media-item-card',
+  templateUrl: './media-item-card.component.html',
+  styleUrls: ['./media-item-card.component.scss']
+})
+export class MediaItemCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
