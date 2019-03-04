258fea9729d4fae9d4a4c7c266689fae83232bb9
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-04 11:57:29 +0200

    #12 Add basic input for search & set up 2-way binding for search term

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -8,10 +8,16 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ToolbarComponent implements OnInit {
   @Input() showSearch: boolean;
   @Input() searchTerm: string;
+  @Output() searchTermChange = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  update() {
+    this.searchTermChange.emit(this.searchTerm);
   }
 
 }
--- src/app/toolbar/toolbar.module.ts
+++ src/app/toolbar/toolbar.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { ToolbarComponent } from './toolbar.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ToolbarComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, FormsModule],
   exports: [ToolbarComponent]
 })
 export class ToolbarModule { }
