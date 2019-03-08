0b15d8722ded44588452f99467c3b81d8c271ad3
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-08 11:52:52 +0200

    Close #16, Finished profile settings

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { User } from '../interfaces/user';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,11 +10,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  user: User;
+  available = true;
+  username;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private userService: UserService, private toast: ToastController) {
   }
 
   ngOnInit() {
+    try {
+      this.user = <User>JSON.parse(localStorage.getItem('user'));
+      this.username = this.user.username;
+    } catch (e) {
+      console.log(e);
+    }
+    if (!this.user) {
+      this.userService.getUserData().subscribe((data) => {
+        this.user = data;
+        localStorage.setItem('user', JSON.stringify(this.user));
+      });
+    }
   }
 
   goBack() {
@@ -19,7 +37,37 @@ export class SettingsPage implements OnInit {
   }
 
   save() {
+    this.userService.updateUser(this.user).subscribe((data) => {
+      this.showToast(data.message).catch(err => console.log(err));
+      if (data.ok) {
+        localStorage.setItem('user', JSON.stringify(this.user));
+      }
+    }, error1 => console.log(error1));
+  }
+
+  uploadPhoto() {
 
   }
 
+  checkUsername(username: string) {
+    if (this.username === username) {
+      return;
+    }
+    if (username.length < 3) {
+      return;
+    }
+    this.userService.checkUsername(username).subscribe(data => {
+      console.log(data);
+      this.available = data.available;
+    });
+  }
+
+  async showToast(message: string) {
+    const toast = await this.toast.create({
+      message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 }
