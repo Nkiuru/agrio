2fd8289306b262ce60373db38f7784e1ddfabaa2
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-11 14:18:00 +0200

    upleasant hack to fix wrong posts being loaded in user profile

--- src/app/components/user-posts/user-posts.component.ts
+++ src/app/components/user-posts/user-posts.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../../interfaces/post';
 import { MediaService } from '../../media.service';
 import { Events } from '@ionic/angular';
@@ -17,14 +17,10 @@ import { User } from '../../interfaces/user';
 export class UserPostsComponent implements OnInit, OnDestroy {
   @Input() user: User;
   postArray: Post[];
-  profilePicArray: Post[];
 
   constructor(private media: MediaService, private event: Events) {
     event.subscribe(EVENT_USER_MEDIA_ARRAY_UPDATE, array => {
       this.postArray = array;
-    });
-    event.subscribe(EVENT_PROFILE_PIC_ARRAY_UPDATE, array => {
-      this.profilePicArray = array;
     });
   }
 
@@ -34,6 +30,5 @@ export class UserPostsComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.event.unsubscribe(EVENT_MEDIA_ARRAY_UPDATE);
-    this.event.unsubscribe(EVENT_PROFILE_PIC_ARRAY_UPDATE);
   }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { StatusBarService } from '../status-bar.service';
+import { MediaService } from '../media.service';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-tabs',
@@ -9,10 +11,16 @@ import { StatusBarService } from '../status-bar.service';
 })
 export class TabsPage {
 
-  constructor(private statusBar: StatusBarService) {}
+  constructor(private statusBar: StatusBarService, private media: MediaService) {
+  }
 
   ionViewWillEnter() {
     this.statusBar.setToDark();
+  }
+
+  loadProfileData() {
+    const user = <User>JSON.parse(localStorage.getItem('user'));
+    this.media.initProfileData(user.user_id);
   }
 
 }
