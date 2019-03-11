2cc26885865ab984294479d20b3d2d0b467edd76
Nkiuru <niklas.kiuru1@gmail.com>
2019-03-11 09:31:15 +0200

    add placeholder image in base64

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,9 @@ const routes: Routes = [
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpPageModule' },
   { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule'},
-  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' }
+  { path: 'user/:id', loadChildren: './user/user.module#UserPageModule' },
+  { path: 'upload/status', loadChildren: './status-update/status-update.module#StatusUpdatePageModule' },
+  { path: 'upload/recipe', loadChildren: './recipe/recipe.module#RecipePageModule' },
 ];
 
 @NgModule({
